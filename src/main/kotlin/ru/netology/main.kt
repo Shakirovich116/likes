cc4be3fe8514ed3fe2f0510f803b19a3ab2e9906
Shakirovich116 <shakirovich116@gmail.com>
2022-04-25 18:39:50 +0300

    Изменен

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,4 +1,5 @@
 package ru.netology
+
 fun main() {
     print("Введите число людей: ")
     val likes :UInt = readLine()!!.toUInt()
@@ -10,5 +11,9 @@ fun main() {
         println("Понравилось троим")
     if (likes == 4u)
         println("Понравилось четверым")
-    else println("Понравилось $likes человекам")
+    if ((likes == 11u) || (likes == 21u) || (likes == 31u) || (likes == 41u) || (likes == 51u) || (likes == 61u) || (likes == 71u) || (likes == 81u))
+        println("Понравилось $likes человеку")
+          else {
+        println("Понравилось $likes человекам")
+    }
 }
